f70426418a7ac9d960e5312cd6db3002df75f7a0
anatawa12 <anatawa12@icloud.com>
2022-07-26 17:39:47 +0900

    initial commit

---
+++ src/main/java/com/anatawa12/fixrtm/magmaNasshornProblem/MagmaNashornProblem.java
@@ -0,0 +1,22 @@
+package com.anatawa12.fixrtm.magmaNasshornProblem;
+
+import net.minecraft.launchwrapper.Launch;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(
+        modid = MagmaNashornProblem.MOD_ID,
+        name = MagmaNashornProblem.MOD_NAME,
+        version = MagmaNashornProblem.VERSION
+)
+public class MagmaNashornProblem {
+    public static final String MOD_ID = "magma-nashorn-problem-reproduce";
+    public static final String MOD_NAME = "Magma Nashorn Problem Reproduce";
+    public static final String VERSION = "1.0-SNAPSHOT";
+
+    @Mod.EventHandler
+    public void preinit(FMLPreInitializationEvent event) {
+        Launch.classLoader.addClassLoaderExclusion("jdk.nashorn.");
+        new jdk.nashorn.api.scripting.NashornScriptEngineFactory().getScriptEngine();
+    }
+}
