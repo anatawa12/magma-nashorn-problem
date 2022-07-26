3ac4bdd53e496b76b98b311ef33a9208695fcba2
anatawa12 <anatawa12@icloud.com>
2022-07-26 17:48:38 +0900

    version 1.0

--- src/main/java/com/anatawa12/fixrtm/magmaNasshornProblem/MagmaNashornProblem.java
+++ src/main/java/com/anatawa12/fixrtm/magmaNasshornProblem/MagmaNashornProblem.java
@@ -12,7 +12,7 @@ import net.minecraftforge.fml.common.Mod;
 public class MagmaNashornProblem {
     public static final String MOD_ID = "magma-nashorn-problem-reproduce";
     public static final String MOD_NAME = "Magma Nashorn Problem Reproduce";
-    public static final String VERSION = "1.0-SNAPSHOT";
+    public static final String VERSION = "1.0";
 
     @Mod.EventHandler
     public void preinit(FMLPreInitializationEvent event) {
